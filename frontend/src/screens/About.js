f9d300553eb3f0413bd97c26e01da099c1a01ae4
mmackenzie-syd <mmackenzie.syd@gmail.com>
2020-11-13 19:54:14 +1100

    add about page

--- frontend/src/screens/About.js
+++ frontend/src/screens/About.js
@@ -1,50 +1,53 @@
 import './About.css';
+import Loading from "../components/Loading";
+import Breadcrumb from "../components/Breadcrumb";
+import Selector from "../components/Selector";
+import React, {Fragment} from "react";
 
 function About() {
     return (
-        <main className="collections margin-top-2 margin-bottom-1">
-            <section className="margin-top-2">
-                <ul className="collections__breadcrumb">
-                    <li className="collections__breadcrumb-item">Home Page</li>
-                    <li className="collections__breadcrumb-item">/</li>
-                    <li className="collections__breadcrumb-item">Shipping and Payment</li>
-                </ul>
-            </section>
-            <section className="about">
-                <h2 className="about__title margin-bottom-5">About us, silver jewelry</h2>
-                <p className="about__paragraph">Charm Accessories online store offers minimalist silver jewelry and
-                    jewelry at affordable prices. We do not just sell jewelry, we create an image and mood.</p>
-                <p className="about__paragraph">The idea of affordable beauty in minimalism and simplicity is our anchor
-                    in the selection and creation of jewelry.</p>
-                <p className="about__paragraph">Charm Accessories offers a large selection of silver jewelry, which is
-                    an affordable and valuable jewelry material. All jewelry is made of 925 sterling silver, plated with
-                    rhodium to protect them from darkening.</p>
-                <p className="about__paragraph">Everything around us is changing rapidly, especially fashion trends.
-                    Earlier, decorations emphasized their status, importance in society. Today, jewelry or accessories
-                    for a person are primarily an element of mood, a way to complement an image. Therefore, a modern
-                    woman tends to choose original, classic or minimalist accessories and jewelry, often change them
-                    depending on the mood, time of day, season, and we are always ready to help with this.</p>
-                <p className="about__paragraph">Silver jewelry is both simple and sophisticated complementing the image
-                    harmoniously, and a successful combination of jewelry, such as silver earrings and bracelets or a
-                    pendant and a silver ring, you can easily create a new image, change your mood. There are also
-                    interesting silver sets in the Charm Accessories online store.</p>
-                <p className="about__paragraph">Our special story is unique bracelets made from natural stones . Created
-                    by the designer Charm Accessories, they have their own message - they repeat natural patterns from
-                    different parts of the world.</p>
-                <p className="about__paragraph">Each of our jewelry is created to emphasize female beauty and
-                    originality, complement the image and create a mood. ...</p>
-                <p className="about__paragraph">The Charm Accessories online store guarantees its customers:</p>
-                    <ul className="about__list">
-                        <li className="about__list-item">professional service</li>
-                        <li className="about__list-item">quality stylish jewelry</li>
-                        <li className="about__list-item">loyal democratic prices</li>
-                        <li className="about__list-item">branded minimalist packaging</li>
-                        <li className="about__list-item">fast delivery throughout Ukraine</li>
-                        <li className="about__list-item">different discounts and promotions</li>
-                    </ul>
-                <p className="about__paragraph">We do not just offer high-quality stylish jewelry, we help to create
-                    a beautiful image and good mood!</p>
-            </section>
+        <main className="product margin-top-5" style={{minHeight: '500px'}}>
+                <section className="row top">
+                    <div className="col-6 product__img">
+                        <img src={`${process.env.PUBLIC_URL}/images/sydney-med.jpg`}/>
+                    </div>
+                    <div className="col-6 padding-left-3">
+                        <div>
+                            <Breadcrumb
+                                list={[{name: 'Home Page', url: '/'}, {name: 'About Us', url: ''}]}
+                                show={true}
+                            />
+                        </div>
+                        <h2 className="product__title margin-bottom-1">About Us</h2>
+                        <h3 className="product__brand-title margin-bottom-2">Charm Accessories</h3>
+                        <p className="product__description margin-top-4">Located in the beautiful harbour city of Sydney,
+                            Charm Accessories online store offers minimalist fashion jewelry and jewelry at affordable prices,
+                            shipping Australia wide. We do not just sell jewelry, we create an image and mood.</p>
+                        <p className="product__description">Everything around us is changing rapidly, especially fashion trends.
+                            Earlier, decorations emphasized their status, importance in society. Today, jewelry or accessories
+                            for a person are primarily an element of mood, a way to complement an image. Therefore, a modern
+                            woman tends to choose original, classic or minimalist accessories and jewelry, often change them
+                            depending on the mood, time of day, season, and we are always ready to help with this.</p>
+                        <p className="product__description">Fashion jewelry is both simple and sophisticated complementing the image
+                            harmoniously, and a successful combination of jewelry, such as silver earrings and bracelets or a
+                            pendant and a silver ring, you can easily create a new image, change your mood. There are also
+                            interesting silver sets in the Charm Accessories online store.</p>
+                        <div className="product__share-container">
+                            <div className="product__share margin-bottom-2">Share this</div>
+                            <div className="social-icons">
+                                <div className="social-icons__img-wrap">
+                                    <img className="social-icons__img" src={`${process.env.PUBLIC_URL}/images/facebook.png`} />
+                                </div>
+                                <div className="social-icons__img-wrap">
+                                    <img className="social-icons__img" src={`${process.env.PUBLIC_URL}/images/instagram.png`} />
+                                </div>
+                                <div className="social-icons__img-wrap">
+                                    <img className="social-icons__img" src={`${process.env.PUBLIC_URL}/images/youtube.png`} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
         </main>
     );
 }
