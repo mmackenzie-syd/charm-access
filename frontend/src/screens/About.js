501a83c16fe51aecbbb19f3137b0b452359904be
mmackenzie-syd <mmackenzie.syd@gmail.com>
2020-12-26 01:52:57 +1100

    refactor about

--- frontend/src/screens/About.js
+++ frontend/src/screens/About.js
@@ -1,4 +1,3 @@
-import './About.css';
 import Breadcrumb from "../components/Breadcrumb";
 import React from "react";
 
@@ -7,26 +6,26 @@ function About() {
         <main className="product margin-top-5 margin-bottom-5" style={{minHeight: '500px'}}>
                 <section className="row top">
                     <div className="col-6">
-                        <img alt="Sydney" className="about__img" src={`${process.env.PUBLIC_URL}/images/sydney.jpg`}/>
+                        <img alt="Sydney" className="product-img" src={`${process.env.PUBLIC_URL}/images/sydney.jpg`}/>
                     </div>
                     <div className="col-6 padding-left-3">
-                        <div>
+                        <div className="margin-bottom-1">
                             <Breadcrumb
                                 list={[{name: 'Home Page', url: '/'}, {name: 'About Us', url: ''}]}
                                 show={true}
                             />
                         </div>
-                        <h2 className="product__title margin-bottom-1">About Us</h2>
-                        <h3 className="product__brand-title margin-bottom-2">Charm Accessories</h3>
-                        <p className="product__description margin-top-4">Located in the beautiful harbour city of Sydney,
+                        <h3 className="margin-bottom-1">About Us</h3>
+                        <h3 className="product-brand-title  margin-bottom-2">Charm Accessories</h3>
+                        <p className="product-description margin-top-4">Located in the beautiful harbour city of Sydney,
                             Charm Accessories online store offers minimalist fashion jewelry and jewelry at affordable prices,
                             shipping Australia wide. We do not just sell jewelry, we create an image and mood.</p>
-                        <p className="product__description">Everything around us is changing rapidly, especially fashion trends.
+                        <p className="product-description">Everything around us is changing rapidly, especially fashion trends.
                             Earlier, decorations emphasized their status, importance in society. Today, jewelry or accessories
                             for a person are primarily an element of mood, a way to complement an image. Therefore, a modern
                             woman tends to choose original, classic or minimalist accessories and jewelry, often change them
                             depending on the mood, time of day, season, and we are always ready to help with this.</p>
-                        <p className="product__description">Fashion jewelry is both simple and sophisticated complementing the image
+                        <p className="product-description">Fashion jewelry is both simple and sophisticated complementing the image
                             harmoniously, and a successful combination of jewelry, such as silver earrings and bracelets or a
                             pendant and a silver ring, you can easily create a new image, change your mood. There are also
                             interesting silver sets in the Charm Accessories online store.</p>
