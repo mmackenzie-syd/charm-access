d9c53504e6b47924533d7d1ac013f64f53db5508
mmackenzie-syd <mmackenzie.syd@gmail.com>
2020-12-27 16:26:26 +1100

    refactor search css

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,28 +1,28 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 import './Search.css';
 import {ModalContext} from "../context/modalContext";
 import SearchIcon from "../icons/SearchIcon";
 
 function Search() {
     let { handleModal } = React.useContext(ModalContext);
-    return (
-        <form className="modal-content animate" method="post">
-            <div className="imgcontainer">
-                <span onClick={() => handleModal('')} className="close" title="Close Modal">&times;</span>
-            </div>
-            <div className="search-container">
-                <div className="input-wrap row">
-                    <input className="search-input" type="text" placeholder="Search..." required />
-                    {/*  <button type="submit" className="search-btn">
-                            <SearchIcon
-                                width={'2rem'}
-                                height={'2rem'}
-                                offset={'.1rem'}
-                            />
-                    </button> */}
-                </div>
+    const [searchTxt, setSearchTxt] = useState('');
 
+    const handleSearch = () => {
+        console.log('handle search', searchTxt);
+    }
+    return (
+        <form className="modal-content animate" onSubmit={handleSearch}>
+            <div className="search-container">
+                <span onClick={() => handleModal('')} className="close" title="Close Modal">&times;</span>
+                <input
+                    className="search-input"
+                    type="text"
+                    placeholder="Search..."
+                    required
+                    value={searchTxt}
+                    onChange={e => setSearchTxt(e.target.value)}
+                />
             </div>
         </form>
     );
