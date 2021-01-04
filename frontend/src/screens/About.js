e967d81a31c0a1ad78744ec066fb2aba674b0200
mmackenzie-syd <mmackenzie.syd@gmail.com>
2021-01-04 20:21:58 +1100

    correct styling for about

--- frontend/src/screens/About.js
+++ frontend/src/screens/About.js
@@ -1,22 +1,39 @@
 import Breadcrumb from "../components/Breadcrumb";
 import React from "react";
+import LeftArrowIcon from "../icons/LeftArrowIcon";
+import RightArrowIcon from "../icons/RightArrowIcon";
+import Selector from "../components/Selector";
+import ArrivalsSlide from "../components/ArrivalsSlide";
 
 function About() {
     return (
-        <main className="product margin-top-5 margin-bottom-5" style={{minHeight: '500px'}}>
-                <section className="row top">
-                    <div className="col-6">
-                        <img alt="Sydney" className="product-img" src={`${process.env.PUBLIC_URL}/images/sydney.jpg`}/>
+        <main className="product" style={{minHeight: '500px'}}>
+                <section className="grid grid-col-2-large">
+                    <div className="product-img-rel">
+                        <img
+                            alt="Sydney"
+                            className="product-img"
+                            src={`${process.env.PUBLIC_URL}/images/sydney.jpg`}
+                        />
                     </div>
-                    <div className="col-6 padding-left-3">
-                        <div className="margin-bottom-1">
-                            <Breadcrumb
-                                list={[{name: 'Home Page', url: '/'}, {name: 'About Us', url: ''}]}
-                                show={true}
-                            />
+                    <div className="">
+                        <div>
+                            <div className="product-desktop">
+                                <Breadcrumb
+                                    list={[{name: 'Home Page', url: '/'}, {name: 'About Us', url: ''}]}
+                                    show={true}
+                                />
+                            </div>
+                            <div className="product-desktop">
+                                <div className="row product-title">
+                                    <h3>About Us</h3>
+                                </div>
+                            </div>
+                            <div className="product-title margin-top-2 product-mobile">
+                                <h3 className="">About Us</h3>
+                            </div>
+                            <h4 className="product-brand-title product-desktop">Charm Accessories</h4>
                         </div>
-                        <h3 className="margin-bottom-1">About Us</h3>
-                        <h3 className="product-brand-title  margin-bottom-2">Charm Accessories</h3>
                         <p className="margin-top-4">Located in the beautiful harbour city of Sydney,
                             Charm Accessories online store offers minimalist fashion jewelry and jewelry at affordable prices,
                             shipping Australia wide. We do not just sell jewelry, we create an image and mood.</p>
@@ -29,22 +46,24 @@ function About() {
                             harmoniously, and a successful combination of jewelry, such as silver earrings and bracelets or a
                             pendant and a silver ring, you can easily create a new image, change your mood. There are also
                             interesting silver sets in the Charm Accessories online store.</p>
-                        <div className="product__share-container">
-                            <div className="product__share margin-bottom-2">Share this</div>
+                        <div>
+                            <div className="product-share margin-bottom-2">Share this</div>
                             <div className="social-icons">
-                                <div className="social-icons__img-wrap">
-                                    <img alt="facebook" className="social-icons__img" src={`${process.env.PUBLIC_URL}/images/facebook.png`} />
+                                <div className="social-icons-img-wrap">
+                                    <img alt="facebook" className="social-icons-img" src={`${process.env.PUBLIC_URL}/images/facebook.png`} />
                                 </div>
-                                <div className="social-icons__img-wrap">
-                                    <img alt="instagram" className="social-icons__img" src={`${process.env.PUBLIC_URL}/images/instagram.png`} />
+                                <div className="social-icons-img-wrap">
+                                    <img alt="instagram" className="social-icons-img" src={`${process.env.PUBLIC_URL}/images/instagram.png`} />
                                 </div>
-                                <div className="social-icons__img-wrap">
-                                    <img alt="youtube" className="social-icons__img" src={`${process.env.PUBLIC_URL}/images/youtube.png`} />
+                                <div className="social-icons-img-wrap">
+                                    <img alt="youtube" className="social-icons-img" src={`${process.env.PUBLIC_URL}/images/youtube.png`} />
                                 </div>
                             </div>
                         </div>
                     </div>
                 </section>
+                <div className="margin-bottom-5"></div>
+
         </main>
     );
 }
